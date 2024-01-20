5174c71dc3909ba5c4e697dd255d431051a0e4d9
Sebastian Ortmann <s.ortmann.hro@gmail.com>
2024-01-20 14:00:13 +0100

    Init example

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for Work Clock integration."""
---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for Work Clock tests."""
+from custom_components.work_clock.const import (
+    CONF_PASSWORD,
+)
+from custom_components.work_clock.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
